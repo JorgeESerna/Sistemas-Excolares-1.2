1633655026c1fd1f7736c8c1a6592ea37d464ac0
jorge ernesto serna navarro <jorgegofy@hotmail.com>
2019-04-27 14:22:22 -0700

    Tarea

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_Id = new System.Windows.Forms.Label();
+            this.tb_matricula = new System.Windows.Forms.TextBox();
+            this.tb_ap1 = new System.Windows.Forms.TextBox();
+            this.lb_precio = new System.Windows.Forms.Label();
+            this.tb_ap2 = new System.Windows.Forms.TextBox();
+            this.lb_url = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllHeaders;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Raised;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Location = new System.Drawing.Point(4, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 400);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Productos";
+            // 
+            // lb_Id
+            // 
+            this.lb_Id.AutoSize = true;
+            this.lb_Id.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Id.Location = new System.Drawing.Point(3, 447);
+            this.lb_Id.Name = "lb_Id";
+            this.lb_Id.Size = new System.Drawing.Size(25, 21);
+            this.lb_Id.TabIndex = 2;
+            this.lb_Id.Text = "Id";
+            // 
+            // tb_matricula
+            // 
+            this.tb_matricula.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_matricula.Location = new System.Drawing.Point(108, 444);
+            this.tb_matricula.Name = "tb_matricula";
+            this.tb_matricula.Size = new System.Drawing.Size(100, 29);
+            this.tb_matricula.TabIndex = 3;
+            this.tb_matricula.Text = "81004";
+            // 
+            // tb_ap1
+            // 
+            this.tb_ap1.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ap1.Location = new System.Drawing.Point(108, 492);
+            this.tb_ap1.Name = "tb_ap1";
+            this.tb_ap1.Size = new System.Drawing.Size(100, 29);
+            this.tb_ap1.TabIndex = 5;
+            this.tb_ap1.Text = "Serna";
+            // 
+            // lb_precio
+            // 
+            this.lb_precio.AutoSize = true;
+            this.lb_precio.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_precio.Location = new System.Drawing.Point(3, 492);
+            this.lb_precio.Name = "lb_precio";
+            this.lb_precio.Size = new System.Drawing.Size(58, 21);
+            this.lb_precio.TabIndex = 4;
+            this.lb_precio.Text = "Precio";
+            // 
+            // tb_ap2
+            // 
+            this.tb_ap2.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ap2.Location = new System.Drawing.Point(108, 540);
+            this.tb_ap2.Name = "tb_ap2";
+            this.tb_ap2.Size = new System.Drawing.Size(100, 29);
+            this.tb_ap2.TabIndex = 7;
+            this.tb_ap2.Text = "Navarro";
+            // 
+            // lb_url
+            // 
+            this.lb_url.AutoSize = true;
+            this.lb_url.Font = new System.Drawing.Font("Ebrima", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_url.Location = new System.Drawing.Point(3, 543);
+            this.lb_url.Name = "lb_url";
+            this.lb_url.Size = new System.Drawing.Size(33, 21);
+            this.lb_url.TabIndex = 6;
+            this.lb_url.Text = "Url";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(233, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 29);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(233, 493);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 29);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Id";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Nombre";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Precio";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Url";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 643);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_ap2);
+            this.Controls.Add(this.lb_url);
+            this.Controls.Add(this.tb_ap1);
+            this.Controls.Add(this.lb_precio);
+            this.Controls.Add(this.tb_matricula);
+            this.Controls.Add(this.lb_Id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Base de datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_Id;
+        private System.Windows.Forms.TextBox tb_matricula;
+        private System.Windows.Forms.TextBox tb_ap1;
+        private System.Windows.Forms.Label lb_precio;
+        private System.Windows.Forms.TextBox tb_ap2;
+        private System.Windows.Forms.Label lb_url;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection mySqlConnection;
+        MySqlCommand mySqlCommand;
+        MySqlDataReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lb_Id.Enabled = false;
+            lb_Id.Text = dataGridView1[0,e.RowIndex].Value.ToString();
+            lb_precio.Text = dataGridView1[1, e.RowIndex].Value.ToString();
+            lb_url.Text = dataGridView1[2, e.RowIndex].Value.ToString();
+        
+        }
+
+        private void cargaDatos()
+        {
+            try
+            {
+                dataGridView1.Rows.Clear();
+
+                mySqlConnection = new MySqlConnection("host=localhost;user=root;password=admin;database=punto_de_venta"); //Streing de conección
+
+                mySqlConnection.Open();
+
+                mySqlCommand = new MySqlCommand("SELECT * FROM productos", mySqlConnection);
+
+                reader = mySqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    dataGridView1.Rows.Add(reader.GetUInt32(0),reader.GetString(1),reader.GetString(2),reader.GetString(3));
+                }
+
+                
+                mySqlConnection.Close();
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.ToString(),"error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargaDatos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = "INSERT INTO alumnos (producto_id,producto_nombre,producto_precio,producto_url VALUES ("+ lb_Id.Text+",'"+ lb_precio.Text + "','"+ lb_url.Text + ";";
+            
+
+            try
+            {
+                mySqlConnection = new MySqlConnection("host=localhost;user=root;password=admin;database=sistemas_escolar"); //Streing de conección
+
+                mySqlConnection.Open();
+                                           
+                mySqlCommand = new MySqlCommand(query, mySqlConnection);
+
+                mySqlCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Finalizado");
+
+                cargaDatos();
+                                
+
+                mySqlConnection.Close();
+                            
+
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.ToString(), "error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String query = "delete from productos where producto_id=" + tb_matricula.Text;
+            
+            //hola mundo
+            try
+            {
+                mySqlConnection = new MySqlConnection("host=localhost;user=root;password=admin;database=sistemas_escolar"); //Streing de conección
+
+                mySqlConnection.Open();
+                                
+                mySqlCommand = new MySqlCommand(query, mySqlConnection);
+
+
+                mySqlCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Finalizado");
+
+                cargaDatos();
+
+                mySqlConnection.Close();
+                              
+                
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString(), "error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Hand);
+            }
+        }
+    }
+}
